ceaf9474f20cd8794d5a4d7cb346ce09dea1d3b2
Павел Беликов <p.belikov@inventos.ru>
2022-02-17 19:12:11 +0300

    get_knn argparse fix
    Former-commit-id: 0eba6ad4fe01c34f729970a8420e4e7d7a576199

--- get_knn.py
+++ get_knn.py
@@ -12,12 +12,43 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy import expand_dims
 import pickle
+import argparse
+
+def get_arguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "-mp",
+        "--model_path",
+        required=True,
+        help="Path to .h5 keras model."
+    )
+    ap.add_argument(
+        "-tip",
+        "--test_image_path",
+        required=True,
+        help="Path to test image."
+    )
+    ap.add_argument(
+        "-pfp",
+        "--pickle_file_path",
+        required=True,
+        help="Path to .pickle data file."
+    )
+    ap.add_argument(
+        "--min_dist",
+        default=0.6,
+        help="Min distanse between classes."
+    )
+    ap.add_argument(
+        "--knn_count",
+        default=10,
+        help="Nearest neighbours count."
+    )
+    return vars(ap.parse_args())
 
 model_path = "./models/model_20211203_rmsprop.h5"   # путь к модели
 test_image = r'/home/pavel/Desktop/University/Diplom/test (not in val)/dataset/Chevrolet Captiva/Chevrolet Captiva 2013_81616476.jpg'       # путь к изображению для распознавания
 pickle_file = "./embedding_data/dataset3.11_val.pickle"               # путь к .pickle файлу сохраненных эмбеддингов
-
-model = load_model(model_path, compile=False)
 
 def get_v(filename, model, required_size=(160, 160)):
   image = Image.open(filename)
@@ -44,34 +75,38 @@ def get_distance(emb1, emb2):
     '''Вычисляет расстояние между векторами.'''
     return np.linalg.norm(emb1 - emb2)/20
 
-def add_distance(name, dist, nearest_list):
+def add_distance(name, dist, nearest_list, n):
     '''Добавляет дистанцию в список ближайших если значение меньше максимального в списке.'''
-    if len(nearest_list) < 10:
+    if len(nearest_list) < n:
         nearest_list.append({"class_name": name, "distance": dist})
         #nearest_list = sorted(nearest_list, key=lambda a: a['distance'], reverse=False)
         return nearest_list
     if dist < nearest_list[-1]["distance"]:
         nearest_list.append({"class_name": name, "distance": dist})
         nearest_list = sorted(nearest_list, key=lambda a: a['distance'],  reverse=False)
-        if len(nearest_list) > 10:
+        if len(nearest_list) > n:
             nearest_list.pop(-1)
     return nearest_list
 
 def print_dist(l):
     for i in l:
-        print("{0}:\t\t {1}".format(i["class_name"], i["distance"]))
+        print("{0}:\t\t\t {1}".format(i["class_name"], i["distance"]))
 
 def main():
+    args = get_arguments()
+    model = load_model(args["model_path"], compile=False)
     nearest_list = []
-    class_embeddings = pickle_to_data(pickle_file)
-    test_embedding = get_v(test_image, model)
+    class_embeddings = pickle_to_data(args["pickle_file_path"])
+    test_embedding = get_v(args["test_image_path"], model)
 
     for i in class_embeddings.keys():
         #if i == "Chevrolet Niva":
         for embedding in class_embeddings[i]:
             distanse = get_distance(test_embedding, embedding)
-            if (distanse < 0.6):
-                nearest_list = add_distance(i, distanse, nearest_list)
+            if (distanse < args["min_dist"]):
+                nearest_list = add_distance(i, distanse, nearest_list, args["knn_count"])
     print_dist(nearest_list)
 
-main()
+
+if __name__ == '__main__':
+    main()
