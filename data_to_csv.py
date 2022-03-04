52c61a994fda2482dd478b590c1f758584c7e339
Павел Беликов <p.belikov@inventos.ru>
2022-03-04 20:14:07 +0300

    ДОбавил скрипт генерации статистики распределения для наборов данных
    Former-commit-id: a845267da33ca509f847bf4e4b32d6448d2f3afa

---
+++ data_to_csv.py
@@ -0,0 +1,81 @@
+import os
+import pickle
+import csv
+import argparse
+import imagesize
+import sys
+
+def get_arguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "--data_path",
+        #required=True,
+        help="Data path."
+    )
+    ap.add_argument(
+        "--filename",
+        required=True,
+        help="Filename to new csv file."
+    )
+    ap.add_argument(
+        "--train_data",
+        default=False,
+        help="Train data flag (if exists)."
+    )
+    ap.add_argument(
+        "--val_data",
+        default=False,
+        help="Val data flag (if exists)."
+    )
+    ap.add_argument(
+        "--test_data",
+        default=False,
+        help="Test data flag (if exists)"
+    )
+    return vars(ap.parse_args())
+
+def write_csv(filename, samples):
+    '''
+    Записывает данные в csv файл.
+    Аргументы:
+        filename - название файла для записи, будет располагаться в папке ./data/;
+        samples - dict, содержащий данные в формате {'выборка данных': 'путь до выборки'} (прим. {'train': './data/train', })
+    '''
+    f = open("./data/" + filename + ".csv", "w+", encoding="UTF8")
+    writer = csv.writer(f)
+    writer.writerow(["image_name", "class_name", "height", "width", "sample", "image_path"])
+
+    for sample in samples.keys():
+        classes = os.listdir(samples[sample])
+        classes_count = len(classes)
+        for cl in classes:
+            cl_path = os.path.join(samples[sample], cl)
+            cars = os.listdir(cl_path)
+            cars_count = len(cars)
+            i = 0
+            for car in cars:
+                car_path = os.path.join(cl_path, car)
+                width, height = imagesize.get(car_path)
+                writer.writerow([car, cl, height, width, sample, car_path])
+                i+=1
+                sys.stdout.write('\r' + ' '*50 + '\r')
+                sys.stdout.write("{} - {}/{}".format(cl, i, cars_count))
+                sys.stdout.flush()
+    f.close()
+
+def main():
+    args = get_arguments()
+    samples = {}
+
+    if args["train_data"]:
+        samples["train"] = os.path.join(args["data_path"], "train/")
+    if args["val_data"]:
+        samples["val"] = os.path.join(args["data_path"], "val/")
+    if args["test_data"]:
+        samples["test"] = os.path.join(args["data_path"], "test/")
+
+    write_csv(args['filename'], samples)
+
+
+if __name__ == '__main__':
+    main()
