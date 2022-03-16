88343e15fa3df7eabed6cba337fe72ec55ca831a
Павел Беликов <p.belikov@inventos.ru>
2022-03-16 10:58:01 +0300

    new test commit

--- vizualize_cluster.py
+++ vizualize_cluster.py
@@ -92,7 +92,8 @@ def main():
     plt.scatter(vis_x, vis_y, c=b, cmap=plt.cm.get_cmap("jet", classes_count), marker='.')
     plt.colorbar(ticks=range(classes_count))
     plt.clim(-0.5, classes_count-0.5)
-    plt.savefig(args['filename'], dpi=300)
+    filepath = './cluster_images/' + args['filename'] + '.png'
+    plt.savefig(filepath, dpi=300)
     #plt.show()
 
 if __name__ == '__main__':
