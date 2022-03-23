b9974e2f1893af79018dfb5991229b19cd8ecf81
Павел Беликов <p.belikov@inventos.ru>
2022-03-23 18:27:13 +0300

    Переделал скрипт тестирования jetson под tflite_runtime и обработку всего одного изображения в цикле

--- tf_jetson_test.py
+++ tf_jetson_test.py
@@ -2,12 +2,12 @@ import os
 import sys
 from PIL import Image
 from numpy import asarray
-#from tensorflow.keras.preprocessing import image
 import numpy as np
 from numpy import expand_dims
 import argparse
 import time
 import tflite_runtime.interpreter as tflite
+
 
 
 def get_arguments():
@@ -21,8 +21,9 @@ def get_arguments():
     )
     ap.add_argument(
         "--data_path",
-        default="./data/dataset3.11_new_split/val/GAZ Volga/",
-        help="Path to class directory."
+        required=True,
+        #default="./data/dataset3.11_new_split/val/GAZ Volga/",
+        help="Path to class directory. (but now a path only to a one image)"
     )
     return vars(ap.parse_args())
 
@@ -50,13 +51,27 @@ def main():
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    count = len(os.listdir(args['data_path']))
-    for image in os.listdir(args['data_path']):
-        image_path = os.path.join(args['data_path'], image)
+    #count = len(os.listdir(args['data_path']))
+    #for #image in os.listdir(args['data_path']):
+        #image_path = os.path.join(args['data_path'], image)
+        #start = time.time()
+
+        #input_data = np.array(prep(image_path), dtype=np.float32)
+
+        #interpreter.set_tensor(input_details[0]['index'], input_data)
+        #interpreter.invoke()
+        #output_data = interpreter.get_tensor(output_details[0]['index'])
+        #a = time.time()-start
+        #print(a)
+        #time_list.append(a)
+        #time_sum += a
+        #print(output_data)
+
+    count = 100
+
+    for i in range(count):
         start = time.time()
-
-        input_data = np.array(prep(image_path), dtype=np.float32)
-
+        input_data = np.array(prep(args['data_path']), dtype=np.float32)
         interpreter.set_tensor(input_details[0]['index'], input_data)
         interpreter.invoke()
         output_data = interpreter.get_tensor(output_details[0]['index'])
@@ -64,7 +79,8 @@ def main():
         print(a)
         time_list.append(a)
         time_sum += a
-        #print(output_data)
+
+
     print("-"*100)
     print("Одно изображение обработано за: ", sum(time_list)/len(time_list))
     print("Изображений в секунду обработано: ", count/time_sum)
