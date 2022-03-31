f49586c5e9ed54e039256ad2fb1dd16a89dc8bc8
Павел Беликов <p.belikov@inventos.ru>
2022-03-31 14:52:47 +0300

    Исправил классификатор и демо для работы на tflite

--- classifier.py
+++ classifier.py
@@ -3,12 +3,12 @@ import math
 import time
 import numpy as np
 
-#from PIL import Image
 import cv2
 
 import pickle
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+import tflite_runtime.interpreter as tflite
+#import tensorflow as tf
+#from tensorflow.keras.models import load_model
 
 def pickle_to_data(fname):
     '''Выгружает данные из файла в словарь.'''
@@ -20,9 +20,9 @@ def pickle_to_data(fname):
 class Classifier:
 
     def __init__(self, model_path: str, pickle_data_path: str, min_distance: int, required_size=(160, 160)):
-        #self.model_path = model_path
+        self.model_path = model_path
         #self.pickle_data_path = pickle_data_path
-        self.model = load_model(model_path, compile=False)
+        #self.model = load_model(model_path, compile=False)
         self.pickle_data = pickle_to_data(pickle_data_path)
         self.required_size = required_size
         self.min_distance = min_distance # задать значение по умолчанию после выяснения данного параметра у текущей модели.
@@ -39,8 +39,9 @@ class Classifier:
         image /= 255
         samples = np.expand_dims(image, axis=0)
         samples = np.stack((samples.copy(),)*3, axis=-1)
-        yhat = self.model.predict(samples)
-        return yhat[0]
+        #yhat = self.model.predict(samples)
+        #return yhat[0]
+        return samples
 
     def add_distance(self, name, dist, nearest_list, n):
         '''Добавляет дистанцию в список ближайших если значение меньше максимального в списке.'''
@@ -57,11 +58,20 @@ class Classifier:
     def get_predict(self, image):
         knn_list = []
         start_time = time.time()
+        interpreter = tflite.Interpreter(model_path=self.model_path)
+        interpreter.allocate_tensors()
+
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
         target_embedding = self.get_embedding(image)
+        input_data = np.array(target_embedding, dtype=np.float32)
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+        output_data = interpreter.get_tensor(output_details[0]['index'])
 
         for car in self.pickle_data.keys():
             for embedding in self.pickle_data[car]:
-                distance = self.get_distance(target_embedding, embedding)
+                distance = self.get_distance(output_data, embedding)
                 if distance < self.min_distance:
                     knn_list = self.add_distance(car, distance, knn_list, 11)
         recognize_time = time.time() - start_time
--- classifier_demo.py
+++ classifier_demo.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-import tensorflow as tf
+#import tensorflow as tf
 import cv2
 import numpy as np
 from classifier import Classifier
@@ -19,12 +19,22 @@ def get_arguments():
         help="Path to test image."
     )
     ap.add_argument(
+        "--test_dir_path",
+        default="./data/d3.11nst/test/",
+        help="Path to test directory with images."
+    )
+    ap.add_argument(
         "-pfp",
         "--pickle_file_path",
         required=True,
         help="Path to .pickle data file."
     )
     return vars(ap.parse_args())
+
+def recall1(class1, class2):
+    if class1 == class2:
+        return 1
+    return 0
 
 def image_to_np(filename):
     image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
@@ -34,7 +44,20 @@ def image_to_np(filename):
 if __name__ == '__main__':
     args = get_arguments()
     o = Classifier(args['model_path'], args['pickle_file_path'], 6)
+    recall_metric = 0
+    recall_count = 0
+    time_list = []
+    time_count = 0
+    for cl in os.listdir(args['test_dir_path']):
+        path = os.path.join(args['test_dir_path'], cl)
+        for car in os.listdir(path):
+            embedding = o.get_predict(image_to_np(os.path.join(path, car)))
+            recall_count += 1
+            time_count += 1
+            recall_metric += recall1(embedding['model'], cl)
+            time_list.append(embedding['recognize_time'])
+    print("RECALL@1 ", recall_metric/recall_count*100)
+    print("Среднее время распознавания изображения ", sum(time_list)/time_count)
+    #image = image_to_np(args['test_image_path'])
 
-    image = image_to_np(args['test_image_path'])
-
-    print(o.get_predict(image))
+    #print(o.get_predict(image))
