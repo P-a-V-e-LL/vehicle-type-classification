9f3ecc22f3140f1cb2e338baae8cd2ecfc876c37
Alexandr Shalimov <a.shalimov@inventos.ru>
2022-05-12 14:02:01 +0300

    131739. Добавил генератор, используемый в соревновании aic.

--- model_training.py
+++ model_training.py
@@ -1,26 +1,17 @@
 '''Обучение нейронной сети.'''
 
-#from sklearn.metrics.pairwise import euclidean_distances
-from PIL import Image
-from numpy import asarray
-import tensorflow as tf
-from scipy.spatial import distance
-from tensorflow.keras.preprocessing import image
-import numpy as np
-from numpy import load
-from numpy import expand_dims
-from numpy import savez_compressed
-from tensorflow.keras.models import load_model
-from sklearn.metrics import accuracy_score
-from tensorflow.keras.models import load_model, clone_model
-import tensorflow_addons as tfa
-from tensorflow_addons.losses.metric_learning import pairwise_distance
-from tensorflow.keras import Model
-from tensorflow.keras.optimizers import Adam, SGD, RMSprop
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import ReduceLROnPlateau
 import argparse
 import os
+import datetime
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+from tensorflow.keras.models import load_model
+from tensorflow_addons.losses.metric_learning import pairwise_distance
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from dataset.balansed_image_dataset import balanced_image_dataset_from_directory
+
 
 def get_arguments():
     ap = argparse.ArgumentParser()
@@ -43,22 +34,25 @@ def get_arguments():
         help="Path to val dataset."
     )
     ap.add_argument(
+        "--epochs",
+        type=int,
+        required=True,
+        help="Training epochs amount."
+    )
+    ap.add_argument(
         "--batch_size",
         type=int,
         default=128,
         help="Batch size."
     )
     ap.add_argument(
-        "--epochs",
-        type=int,
-        required=True,
-        help="Training epochs amount."
+        "--model_name",
+        type=str,
+        default="model_new",
+        help="Название эксперимента."
     )
     return vars(ap.parse_args())
 
-def _normalize_img(img, label):
-    img = tf.cast(img, tf.float32) / 255.
-    return (img, label)
 
 def check(path):
     i = 0
@@ -67,76 +61,118 @@ def check(path):
         i += len(os.listdir(n))
     return i
 
-img_width, img_height = 160, 160 				# целевой размер изображения для обучения
-save_model_path = './models/'             # путь к месту сохранения обученной модели
-
 
 def triplet_accuracy(y_true, y_pred):
     batch_size =tf.cast(tf.size(y_true), dtype=tf.dtypes.float32)
-    # Build pairwise squared distance matrix
-    pdist_matrix = pairwise_distance(y_pred, squared=True) # was pairwise_distance
+    # Build pairwise distance matrix
+    pdist_matrix = pairwise_distance(y_pred, squared=False) # was pairwise_distance
     # Build pairwise binary adjacency matrix.
     adjacency = tf.cast(tf.math.equal(y_true, tf.transpose(y_true)), dtype=tf.dtypes.float32)
     # Invert so we can select negatives only.
-    adjacency_not = 1-adjacency
+    adjacency_not = 1 - adjacency
     # Convert to decision with thresholding at 0.5
     predicted = tf.cast(tf.math.less_equal(pdist_matrix, 0.5), dtype=tf.dtypes.float32)
     # Calculate true positives and true negatives
-    true_trues = tf.reduce_sum(tf.cast(
-        tf.math.multiply(predicted, adjacency)
-        , dtype=tf.dtypes.float32))
-    true_falses = tf.reduce_sum(tf.cast(
-        tf.math.multiply(1-predicted, adjacency_not)
-        , dtype=tf.dtypes.float32))
+    tp = tf.reduce_sum(
+        tf.cast(
+            tf.math.multiply(predicted, adjacency),
+            dtype=tf.dtypes.float32
+        )
+    )
+    tn = tf.reduce_sum(
+        tf.cast(
+            tf.math.multiply(1-predicted, adjacency_not),
+            dtype=tf.dtypes.float32
+        )
+    )
     # Calculate percentage
-    return (true_trues+true_falses)/(batch_size*batch_size)
+    return (tp + tn) / (batch_size * batch_size)
 
 
 def main():
     args = get_arguments()
-    nb_train_samples = check(args['train_data'])#47734
+    seed = 3
+    img_width, img_height = 160, 160 # целевой размер изображения для обучения
+    save_model_path = './models/' # путь к месту сохранения обученной модели
+    #  Train batch (128) = num_classes_per_batch_train * num_images_per_class_train (должно быть кратно 2)
+    num_classes_per_batch_train = 8
+    num_images_per_class_train = 16
+    #  Val batch (64) = num_classes_per_batch_val * num_images_per_class_val (должно быть кратно 2)
+    num_classes_per_batch_val = 8
+    num_images_per_class_val = 8
+    nb_train_samples = check(args['train_data'])
     nb_val_samples = check(args['val_data'])
-    model = load_model(args['model_path'])
-    #model = clone_model(model)
-    #sgd = SGD(lr=0.001, momentum=0.001, nesterov=True)
+    model = load_model(args['model_path'], compile=False)
+    exp_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    exp_name = "_".join(
+        [args['model_name'], exp_time]
+    )
+    log_dir = os.path.join("./logs/", exp_time)
+    # model = clone_model(model)
+    # opt = SGD(lr=0.001, momentum=0.001, nesterov=True)
+    # opt =  Adam(learning_rate=1e-5)
     opt = RMSprop(learning_rate=0.001, centered=True)
 
-    model.compile(loss=tfa.losses.TripletSemiHardLoss(),
-                  optimizer=opt,  #Adam(learning_rate=1e-5),
-    	      metrics=[triplet_accuracy]) # 'accuracy' or triplet_accuracy
+    model.compile(
+        loss=tfa.losses.TripletSemiHardLoss(),
+        optimizer=opt, 
+        metrics=[triplet_accuracy]
+    )
 
-    datagen = ImageDataGenerator(rescale=1. / 255)
+    train_generator = balanced_image_dataset_from_directory(
+        args['train_data'],
+        num_classes_per_batch=num_classes_per_batch_train,
+        num_images_per_class=num_images_per_class_train,
+        image_size=(img_width, img_height),
+        seed=seed,
+        augment=False,
+        safe_triplet=True
+    )
+    val_generator = balanced_image_dataset_from_directory(
+        args['val_data'],
+        num_classes_per_batch=num_classes_per_batch_val,
+        num_images_per_class=num_images_per_class_val,
+        image_size=(img_width, img_height),
+        seed=seed,
+        augment=False,
+        safe_triplet=True
+    )
 
-    train_generator = datagen.flow_from_directory(args['train_data'],
-                                                  target_size=(img_width, img_height),
-                                                  batch_size=args['batch_size'],
-                                                  class_mode='sparse')
-
-    val_generator = datagen.flow_from_directory(args['val_data'],
-                                                  target_size=(img_width, img_height),
-                                                  batch_size=args['batch_size'],
-                                                  class_mode='sparse')
-
-    my_callbacks = tf.keras.callbacks.ModelCheckpoint(filepath=save_model_path + 'model_new_callback.h5',
-                                                       monitor='val_loss',
-                                                       mode='min',
-                                                       save_best_only=True)
-
-    import datetime
-    log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
+    # datagen = ImageDataGenerator(rescale=1. / 255)
+    # train_generator = datagen.flow_from_directory(args['train_data'],
+    #                                               target_size=(img_width, img_height),
+    #                                               batch_size=args['batch_size'],
+    #                                               class_mode='sparse')
+    # val_generator = datagen.flow_from_directory(args['val_data'],
+    #                                               target_size=(img_width, img_height),
+    #                                               batch_size=args['batch_size'],
+    #                                               class_mode='sparse')
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        filepath=os.path.join(save_model_path, f'{exp_name}_best.h5'),
+        monitor='val_loss',
+        mode='min',
+        save_best_only=True
+    )
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir, histogram_freq=1
+    )
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=25, verbose=1)
+    model.fit(
+        train_generator,
+        epochs=int(args['epochs']),
+    	callbacks=[tensorboard_callback, reduce_lr, model_checkpoint],
+        validation_data=val_generator,
+    )
+    # model.fit_generator(
+    #     train_generator,
+    #     steps_per_epoch=nb_train_samples // args['batch_size'],
+    #     epochs=int(args['epochs']),
+    #     callbacks=[tensorboard_callback, reduce_lr, model_checkpoint],
+    #     validation_data=val_generator,
+    #     validation_steps=nb_val_samples // args['batch_size']
+    # )
+    model.save(os.path.join(save_model_path, f'{exp_name}.h5'))
 
-    model.fit_generator(train_generator,
-              steps_per_epoch=nb_train_samples // args['batch_size'],
-              epochs=int(args['epochs']),
-              #callbacks=my_callbacks,
-    	      callbacks=[tensorboard_callback, reduce_lr, my_callbacks],
-              validation_data=val_generator,
-              validation_steps=nb_val_samples // args['batch_size'])
-
-    model.save(save_model_path+'model_new.h5')
 
 if __name__ == '__main__':
     main()
